034fb081ae8d63777e8c482d964c6b9464fb5bb1
Aaron <kyrumx@gmail.com>
2022-03-08 10:05:39 +0100

    Logic to download and save a file

--- src/CSCG3DBAGPipeline/Program.cs
+++ src/CSCG3DBAGPipeline/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CSCG3DBAGPipeline;
+
 Console.WriteLine("Hello, World!");
---
+++ src/CSCG3DBAGPipeline/TestFunctions.cs
@@ -0,0 +1,27 @@
+﻿namespace CSCG3DBAGPipeline;
+
+public static class TestFunctions
+{
+    public static async Task TestDownload()
+    {
+        var downloader = new Downloader(@"E:\Hogeschool Rotterdam\Afstuderen CityGIS\Projects\CSCG3DBAGPipeline\download");
+        string downloadPath = "https://aaronbeetstra.me/static/images/projects/";
+        String[] items = new[]
+        {
+            "ACELOGO_logo_los.png",
+            "cf_mp_pix.png",
+            "fridget-overview.jpg",
+            "fridget-pixelated.png"
+        };
+
+        foreach (var fileN in items)
+        {
+            await downloader.DownloadFile(
+                downloadPath + fileN,
+                fileN);
+        }
+
+        Console.WriteLine("Download done");
+        
+    }
+}
