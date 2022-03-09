217def2668c630657bedadbc91e8d40427499db0
Aaron <kyrumx@gmail.com>
2022-03-09 16:37:10 +0100

    Added B3DM functionality

--- src/CSCG3DBAGPipeline/TestFunctions.cs
+++ src/CSCG3DBAGPipeline/TestFunctions.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-using CliWrap;
-using CSCG3DBAGPipeline.processing;
+﻿using CSCG3DBAGPipeline.processing;
 
 namespace CSCG3DBAGPipeline;
 
@@ -89,6 +87,31 @@ public static class TestFunctions
         var dracores = await glbProcessor.ApplyDracoCompression("glb/6229.glb", "draco glb/6229d.glb");
         
         Console.WriteLine("Done!");
+    }
+    
+    public static async Task TestB3dm()
+    {
+        var workingDir = @"E:\Hogeschool Rotterdam\Afstuderen CityGIS\Projects\CSCG3DBAGPipeline";
+        var processor = new CityJSONProcessor(
+            @"E:\Hogeschool Rotterdam\Afstuderen CityGIS\Projects\CSCG3DBAGPipeline\cjio-upgrade-filter.bat",
+            @"E:\Hogeschool Rotterdam\Afstuderen CityGIS\Projects\CSCG3DBAGPipeline\cjio-glb.bat",
+            workingDir
+        );
 
+        var res = await processor.FirstStep("download/6229.json", "filtered/6229.json");
+
+        processor.MoveMaaiveldToZero("filtered/6229.json", "moved/6229.json");
+
+        var glbres = await processor.SecondStep("moved/6229.json", "glb/6229.glb");
+
+        var glbProcessor = new GLBProcessor(
+            @"E:\Hogeschool Rotterdam\Afstuderen CityGIS\Projects\CSCG3DBAGPipeline\gltf-transform-draco-edgebreaker.bat",
+            workingDir);
+        
+        var dracores = await glbProcessor.ApplyDracoCompression("glb/6229.glb", "draco glb/6229d.glb");
+        
+        glbProcessor.ToBatched3DModel("draco glb/6229d.glb", "b3dm/6229b3dm.b3dm");
+        
+        Console.WriteLine("Done!");
     }
 }
--- src/CSCG3DBAGPipeline/processing/GLBProcessor.cs
+++ src/CSCG3DBAGPipeline/processing/GLBProcessor.cs
@@ -21,7 +21,7 @@ public class GLBProcessor : Processor
     /// Takes a binary GLB file and applies Draco compression. Uses the specified applyDracoFile, which is a .bat.
     /// </summary>
     /// <param name="inFilePath">Filename (with extension) used as input, relative to the working directory.</param>
-    /// <param name="outFilePath">Filename (with extension) used as input, relative to the working directory.</param>
+    /// <param name="outFilePath">Filename (with extension) of output file, relative to the working directory.</param>
     /// <returns>Task type CommandResult</returns>
     public async Task<CommandResult> ApplyDracoCompression(string inFilePath, string outFilePath)
     {
@@ -29,5 +29,27 @@ public class GLBProcessor : Processor
         CommandResult res = await cmd.ExecuteBufferedAsync();
 
         return res;
+    }
+
+    /// <summary>
+    /// Takes a binary GLB file and converts it to a Batched 3D Model.
+    /// </summary>
+    /// <param name="inFilePath">Filename (with extension) used as input, relative to the working directory.</param>
+    /// <param name="outFilePath">Filename (with extension) of output file, relative to the working directory.</param>
+    public void ToBatched3DModel(string inFilePath, string outFilePath)
+    {
+        // Bouw de paden
+        string glbFilePath = Path.Combine(this._workingDirectory, inFilePath);
+        string outB3dmFilePath = Path.Combine(this._workingDirectory, outFilePath);
+        
+        // Lees GLB
+        byte[] glb = File.ReadAllBytes(glbFilePath);
+        
+        // Maak Batched 3D Model
+        var b3dm = new B3dm.Tile.B3dm(glb);
+        var b3dmBytes = b3dm.ToBytes();
+        
+        // Schrijf Batched 3D Model naar het opgegeven pad met de opgegeven naam
+        File.WriteAllBytes(outB3dmFilePath, b3dmBytes);
     }
 }
